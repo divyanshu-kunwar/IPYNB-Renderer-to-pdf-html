055a29b254a94ca8f96fdc7e44b7e8cf1301a960
Divyanshu Kunwar <kdivyanshu3@gmail.com>
2023-06-22 09:13:10 +0530

    trying configuration to rewrite routes

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), mkcert()],
-  server: { https: true },
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    fs: {
+      // Allow serving files from the output directory
+      allow: ['..']
+    },
+    // Enable SPA fallback
+    proxy: {
+      // Proxy all requests to index.html
+      '/': {
+        target: '/index.html',
+        bypass: () => '/index.html'
+      }
     }
   }
 })
